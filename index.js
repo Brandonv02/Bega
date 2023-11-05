0687ead9f31f5b68c79fff78e74e14c212363956
Brandonv02 <83413276+Brandonv02@users.noreply.github.com>
2023-11-05 11:44:33 -0500

    assets implementacion

--- index.js
+++ index.js
@@ -10,10 +10,15 @@ const swaggerUI = require("swagger-ui-express");
 
 require("dotenv").config();
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "./frontend/views"));
+app.set("pages", path.join(__dirname, "./frontend/views/pages"));
+app.use(express.static(__dirname + "/frontend/views/assets"));
 app.use(morgan("dev"));
 app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use(express.json());
+
+app.get("/landing", (req, res) => {
+  res.render("../frontend/views/pages/landing");
+});
 
 app.use("/api", routes);
 
