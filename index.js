b28c6bbed9622b67c120723d479cdd109ee46a20
SergioToroM <126355149+SergioToroM@users.noreply.github.com>
2023-11-22 21:52:32 -0500

    Modificar datos catalogo

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.get("/", async (req, res) => {
   const productos = await buscarProductos();
   console.log(productos, "llefo")
-  res.render("productos", {
+  res.render("landing", {
     produc : productos
   });
 });
