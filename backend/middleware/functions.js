4cde36faa44a5cefda5a0db04be9acf2f5cc358c
Brandonv02 <83413276+Brandonv02@users.noreply.github.com>
2023-12-09 14:36:44 -0500

    ajusto cotizacion

--- backend/middleware/functions.js
+++ backend/middleware/functions.js
@@ -8,20 +8,45 @@ const sendEmail = async (params) => {
   const transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: {
-      user: "bkvides@misena.edu.co",
-      pass: "nieu mofc lkdj igwm",
+      user: "begaproductos@gmail.com",
+      pass: "gape dqlm sqhs xwfo",
     },
   });
   const mailOptions = {
     from: "Remitente",
     to: "brandonvidesd@gmail.com",
     subject: "Solicitud de cotizacion",
-    text: `Nueva cotizacion: El señor(a) ${params.nombre} de la empresa ${params.compania}, esta solicitando una 
-    cotizacon comunicarse al correo: ${params.correo} `,
+    html: `<p style="color: green; font-size: 40px; font-weight: bold;">Nueva cotización:</p>
+    <p>El señor(a) ${params.nombre} de la empresa ${params.compania} está solicitando una cotización.</p>
+    <p>Comuníquese al correo: ${params.correo}</p>
+    <img src="cid:logo" style="width: 200px;" alt="Logo">`,
+    attachments: [
+      {
+        filename: "BEGA.png",
+        path: "./frontend/static/assets/images/BEGA.png",
+        cid: "logo", // Este CID es utilizado en el src del tag img en el HTML
+      },
+    ],
+  };
+  const mailOptions2 = {
+    from: "Remitente",
+    to: params.correo,
+    subject: "Solicitud de cotizacion",
+    html: `<p style="color: green; font-size: 40px; font-weight: bold;">Nueva cotización:</p>
+    <p>Señor(a) ${params.nombre} de la empresa ${params.compania} su solicitud fue enviada correctamente.</p>
+    <img src="cid:logo" style="width: 200px;" alt="Logo">`,
+    attachments: [
+      {
+        filename: "BEGA.png",
+        path: "./frontend/static/assets/images/BEGA.png",
+        cid: "logo", // Este CID es utilizado en el src del tag img en el HTML
+      },
+    ],
   };
 
   try {
     await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions2);
     return "OK";
   } catch (error) {
     console.error("Error al enviar el correo:", error.message);
